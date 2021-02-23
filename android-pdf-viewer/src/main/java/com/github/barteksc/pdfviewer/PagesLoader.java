3df6311e1e7d491427ec2193ce8f40d4915013b8
Federico Chiaravalli <federico.ch@outlook.it>
2021-02-23 15:39:31 +0100

    Fixed blurry page load in case of vertical page, landascape orientation and horizontal scroll and in case of horizontal page, portrait orientation and vertical scroll

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/PagesLoader.java
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/PagesLoader.java
@@ -205,13 +205,13 @@ class PagesLoader {
 
             // calculate the row,col of the point in the leftTop and rightBottom
             if (pdfView.isSwipeVertical()) {
-                range.leftTop.row = MathUtils.floor(Math.abs(pageFirstYOffset - pdfView.pdfFile.getPageOffset(range.page, pdfView.getZoom())) / rowHeight);
+                range.leftTop.row = MathUtils.floor(MathUtils.min(pageFirstYOffset - pdfView.pdfFile.getPageOffset(range.page, pdfView.getZoom()), 0) / rowHeight);
                 range.leftTop.col = MathUtils.floor(MathUtils.min(pageFirstXOffset - secondaryOffset, 0) / colWidth);
 
                 range.rightBottom.row = MathUtils.ceil(Math.abs(pageLastYOffset - pdfView.pdfFile.getPageOffset(range.page, pdfView.getZoom())) / rowHeight);
                 range.rightBottom.col = MathUtils.floor(MathUtils.min(pageLastXOffset - secondaryOffset, 0) / colWidth);
             } else {
-                range.leftTop.col = MathUtils.floor(Math.abs(pageFirstXOffset - pdfView.pdfFile.getPageOffset(range.page, pdfView.getZoom())) / colWidth);
+                range.leftTop.col = MathUtils.floor(MathUtils.min(pageFirstXOffset - pdfView.pdfFile.getPageOffset(range.page, pdfView.getZoom()), 0) / colWidth);
                 range.leftTop.row = MathUtils.floor(MathUtils.min(pageFirstYOffset - secondaryOffset, 0) / rowHeight);
 
                 range.rightBottom.col = MathUtils.floor(Math.abs(pageLastXOffset - pdfView.pdfFile.getPageOffset(range.page, pdfView.getZoom())) / colWidth);
